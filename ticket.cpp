df34890e092b3a2a3045cd0e280fb378abe1f44f
= <duy.ado97@gmail.com>
2018-03-15 17:49:57 -0700

    Update ticket.cpp V1.2

--- ticket.cpp
+++ ticket.cpp
@@ -1,48 +1,57 @@
+/*
+NOTE: using type works better with the function pthread, so you won't get casting warnings
+*/
 #include <iostream>
 #include <pthread.h>
 #include <stdlib.h>
-#define NUMTICKETS 500
+#define NUMTICKETS 1000
 #define NUMSELLER 3
 using namespace std;
-
-//long ticket = NUMTICKETS;
-long ticketNum =NUMTICKETS;
-pthread_mutex_t mut;
-void *buyTicket(void *);
-long randNumTicket();
+long ticketNum =NUMTICKETS;													// number of ticket =1000
+pthread_mutex_t mut;														// our lock
+void *buyTicket(void *);													// void * function prototype
+long randNumTicket();														// rand function prototype
 
 
 int main() {
-	long ranTicket =0;
-	int counter =0;
+	
+	long ranTicket =0;													
+	long buyer =1000; 														// 1000 buyer
 	pthread_t thread[NUMSELLER];
-	while(ticketNum>0){
-		for (int i =0; i<NUMSELLER; i++) {
-			ranTicket=randNumTicket();
-			pthread_create(&thread[i], NULL, buyTicket, (void *) ranTicket);
-		}
-		for (int i =0; i<NUMSELLER; i++) {
-			pthread_join(thread[i], NULL);
-		}
+	
+	for (int i =0; i<NUMSELLER; i++) {
+		ranTicket=randNumTicket();
+		cout<<"thread "<< i << " "<<endl;			
+		pthread_create(&thread[i], NULL, buyTicket, (void *) buyer);		// creating 3 threads, passing in the buyTicket function, and void * buyer( number of buyer)
 	}
-	cout<<"tickNum "<<ticketNum<<endl;
+	for (int i =0; i<NUMSELLER; i++) {										// joining the threads( Basically trash collector **I THINK* NOT TOO SURE)
+		pthread_join(thread[i], NULL);
+	}
+	
+	cout<<"tickNum "<<ticketNum<<endl;										// test to make sure 0 ticket left
+	
 	cout<<"done"<<endl;
 	return 0;
 	
 }
 
-long randNumTicket(){		// generate random #ticket to buy
-	return rand() % 3 + 1;	//rand() % max + min 
+long randNumTicket(){														// generate random #ticket to buy
+	return rand() % 3 + 1;													// rand() % max + min 
 }
-void *buyTicket(void * ranT){
-	long randBuyTicket =  (long) ranT;
-	//pthread_mutex_lock(&mut);
-	
-	if (ticketNum - randBuyTicket>=0){ // check if there are ticket left
-		ticketNum = ticketNum - randBuyTicket;
-		cout <<"sell "<< randBuyTicket <<" "<<ticketNum<<endl;
+void *buyTicket(void * buyeR){
+	long buyerRR =  (long) buyeR;											// long cast void * buyeR to long
+	long randBuy;															// Random number of ticket to for buyer 
+	while (buyerRR >0||ticketNum>0){										// checking if there are ticket left or buyer left 
+		
+		pthread_mutex_lock(&mut);											// locking the thread
+		randBuy = randNumTicket();											// Random number of ticket to for buyer 
+		if (ticketNum - randBuy>=0){ 										// check if there are ticket left 
+			ticketNum = ticketNum - randBuy;								// sell
+			cout <<"sell "<< randBuy <<" "<<ticketNum<<endl;
+		}
+		pthread_mutex_unlock(&mut);											// unlocking the thread
+		buyerRR--;															// minus 1 buyer after every transaction 
 	}
-	//pthread_mutex_unlock(&mut);
 }
 
 
